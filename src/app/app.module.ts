46ace614437d9a64f08f67caa463ff3d4a9ad53d
josecarloslbj <josecarloslbj@gmail.com>
2019-08-31 12:12:30 -0300

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { UserService } from './shared/user.service';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthInterceptor } from './auth/auth.interceptor';
+import { UserComponent } from './user/user.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    RegistrationComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    // BrowserAnimationsModule,
+    // ToastrModule.forRoot({
+    //   progressBar: true
+    // }),
+    FormsModule
   ],
-  providers: [],
+  providers: [UserService, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/user/registration/registration.component.ts
@@ -0,0 +1,49 @@
+import { UserService } from './../../shared/user.service';
+import { Component, OnInit } from '@angular/core';
+// import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styles: []
+})
+export class RegistrationComponent implements OnInit {
+
+  constructor(public service: UserService
+    // , private toastr: ToastrService
+  ) { }
+
+  ngOnInit() {
+    this.service.formModel.reset();
+  }
+
+  onSubmit() {
+    this.service.register().subscribe(
+      (res: any) => {
+        if (res.succeeded) {
+          this.service.formModel.reset();
+          // this.toastr.success('New user created!', 'Registration successful.');
+          alert('New user created!');
+        } else {
+          res.errors.forEach(element => {
+            switch (element.code) {
+              case 'DuplicateUserName':
+                //   this.toastr.error('Username is already taken', 'Registration failed.');
+                alert('Username is already taken');
+                break;
+
+              default:
+                //  this.toastr.error(element.description, 'Registration failed.');
+                alert('element.description');
+                break;
+            }
+          });
+        }
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+}
